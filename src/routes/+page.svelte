580147cee6e7a90abd9159b59de6c9423c83ab13
Invra <identificationsucks@gmail.com>
2026-09-03 03:12:35 +1000

    chore: add initial test stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { ArrowDownToLine } from '@lucide/svelte'
+</script>
+
+
+<main class="py-20 flex flex-col items-center">
+  <div class="flex gap-4 items-center">
+    <img class="w-18 h-18" src="favicon.ico"/>
+    <h1 class="font-bold text-5xl text-primary-header">Froststrap</h1>
+  </div>
+  <p class="text-demuted text-center mt-1">A Roblox bootstrapper forked from Fishstrap<br/> with Linux and macOS support added</p>
+  <div class="flex mt-5 gap-4">
+    <a
+      class="flex gap-2 pl-4.5 bg-primary-header/10
+             hover:bg-primary-header/20 onclick:bg-primary-header
+             text-primary-header font-bold py-3 px-6 rounded-lg"
+      href="https://github.com/froststrap/froststrap/releases/latest"
+    >
+      <ArrowDownToLine/> Download latest
+    </a>
+    <a
+      class="flex gap-2 bg-card-background hover:bg-primary-header/20
+             text-primary-paragraph hover:text-primary-header
+             font-bold py-3 px-6 pl-5 rounded-lg items-center"
+      href="https://github.com/Froststrap/Froststrap"
+    >
+      <svg
+        class="w-5 h-5"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+        />
+      </svg>
+      GitHub
+    </a>
+  </div>
+  <div class="flex gap-4">
+    <div class="bg-card-background rounded-lg w-140 h-90 mt-20"></div>
+    <div class="bg-card-background rounded-lg w-140 h-90 mt-20"></div>
+    <div class="bg-card-background rounded-lg w-140 h-90 mt-20"></div>
+  </div>
+</main>
